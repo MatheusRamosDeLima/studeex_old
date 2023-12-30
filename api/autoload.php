31604daf953ee637fa09813d8f088b34768a20e9
MatheusRamosDeLima <matheusramosej@gmail.com>
2023-12-30 00:30:17 -0300

    Mudança do arquivo vercel.json

--- api/autoload.php
+++ api/autoload.php
@@ -1,6 +1,6 @@
 <?php
     spl_autoload_register(function($fileName) {
-        $path = $_SERVER['DOCUMENT_ROOT'].'/app';
+        $path = $_SERVER['DOCUMENT_ROOT'].'/api';
         if (file_exists("$path/Core/$fileName.php")) require_once "$path/Core/$fileName.php";
         if (file_exists("$path/Controllers/$fileName.php")) require_once "$path/Controllers/$fileName.php";
         if (file_exists("$path/Models/$fileName.php")) require_once "$path/Models/$fileName.php";
