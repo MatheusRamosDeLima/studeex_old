6aae986a0d3b007aa8218914777d04cb3d7118c8
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-02 18:48:38 -0300

    Front: Mais conteúdo e estilo para a página home

--- api/Views/pages/home.php
+++ api/Views/pages/home.php
@@ -1,2 +1,15 @@
-<h1>Seja Bem-Vindo</h1>
-<p>Página principal</p>
+<h1>Escolha o que deseja criar:</h1>
+<section id="systems-create">
+    <a href="/criar/rotina">
+        <div>
+            <img src="" alt="Ícone de rotina">
+            <span>Rotina de Estudos</span>
+        </div>
+    </a>
+    <a href="/criar/ciclo">
+        <div>
+            <img src="" alt="Ícone de ciclo">
+            <span>Ciclo de Estudos</span>
+        </div>
+    </a>
+</section>
