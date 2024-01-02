3a5f6c331f711bce2117e10380b5fb0db151e207
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-02 18:53:05 -0300

    Front: Correção do html da página home

--- api/Views/pages/home.php
+++ api/Views/pages/home.php
@@ -1,15 +1,11 @@
 <h1>Escolha o que deseja criar:</h1>
 <section id="systems-create">
     <a href="/criar/rotina">
-        <div>
-            <img src="" alt="Ícone de rotina">
-            <span>Rotina de Estudos</span>
-        </div>
+        <img src="" alt="Ícone de rotina">
+        <span>Rotina de Estudos</span>
     </a>
     <a href="/criar/ciclo">
-        <div>
-            <img src="" alt="Ícone de ciclo">
-            <span>Ciclo de Estudos</span>
-        </div>
+        <img src="" alt="Ícone de ciclo">
+        <span>Ciclo de Estudos</span>
     </a>
 </section>
