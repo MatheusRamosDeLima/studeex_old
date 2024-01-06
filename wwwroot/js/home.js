57630a44d77445e3270b9fc5196e408a903ba954
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-06 17:37:42 -0300

    Front: Tentativa de adição de responsividade em relação ao menu e seção main

--- wwwroot/js/home.js
+++ wwwroot/js/home.js
@@ -1,2 +1,4 @@
-menuList.classList.add('list-visible');
-mainContent.classList.add('list-visible');
+if (window.innerWidth > 900) {
+    menuList.classList.add('list-visible');
+    mainContent.classList.add('list-visible');
+}
