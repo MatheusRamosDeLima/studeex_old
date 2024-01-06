18bbfeb0c679375fc8dcbc26a9ed994811e609f9
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-06 09:10:14 -0300

    Front: Correção do sistema de menu e main (style e js)

--- wwwroot/js/home.js
+++ wwwroot/js/home.js
@@ -1,2 +1,2 @@
 menuList.classList.add('list-visible');
-// mainContent.classList.add('list-visible');
+mainContent.classList.add('list-visible');
--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,7 +1,7 @@
 const menuList = document.querySelector('#menu-list');
-// const mainContent = document.querySelector('#main-content');
+const mainContent = document.querySelector('#main-content');
 
 function clickMenuButton() {
     menuList.classList.toggle('list-visible');
-    // mainContent.classList.toggle('list-visible');
+    mainContent.classList.toggle('list-visible');
 }
