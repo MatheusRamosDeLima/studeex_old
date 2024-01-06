55b3268df5d1b6863d7d748af3dddde14e06c7c9
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-06 09:33:08 -0300

    Front: Adição de mais estilos para a página home

--- api/Views/pages/home.php
+++ api/Views/pages/home.php
@@ -1,11 +1,14 @@
-<h1>Escolha o que deseja criar:</h1>
-<section id="systems-create">
-    <a href="/criar/rotina">
-        <img src="" alt="Ícone de rotina">
-        <span>Rotina de Estudos</span>
-    </a>
-    <a href="/criar/ciclo">
-        <img src="" alt="Ícone de ciclo">
-        <span>Ciclo de Estudos</span>
-    </a>
+<section id="choose-system">
+    <h1>Escolha o que deseja criar:</h1>
+    <div id="systems-create">
+        <a href="/criar/rotina">
+            <img src="" alt="Ícone de rotina">
+            <h2>Rotina de Estudos</h2>
+        </a>
+        <a href="/criar/ciclo">
+            <img src="" alt="Ícone de ciclo">
+            <h2>Ciclo de Estudos</h2>
+        </a>
+    </div>
 </section>
+<section id="see-systems"></section>
