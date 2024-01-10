cb216074c79dff48477e188b1717e4bdf92f135d
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-01-09 22:54:31 -0300

    Front: Tentativa de correção de nova funcionalidade do menu (nome do link desaparecer e aparecer dependendo de fatores[width, button click])

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,7 @@
 if (window.innerWidth > 900) {
     menuList.classList.add('list-visible');
+    menuListName.forEach(listName => {
+        listName.classList.add('list-visible');
+    });
     mainContent.classList.add('list-visible');
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,11 @@
 const menuList = document.querySelector('#menu-list');
+const menuListName = document.querySelectorAll('.list-name');
 const mainContent = document.querySelector('#main-content');
 
 function clickMenuButton() {
     menuList.classList.toggle('list-visible');
+    menuListName.forEach(listName => {
+        listName.classList.toggle('list-visible');
+    });
     mainContent.classList.toggle('list-visible');
 }
